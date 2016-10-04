b53610d4a1722625fd198712cb6ef1745ae09583
JyotendraPallav <Jyotendra@gmail.com>
2016-10-04 15:48:12 +0530

    Started Fact

---
+++ learnjava2/src/main/java/com/example/CC_Fact_4.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.Scanner;
+
+/**
+ * Created by A0262 on 10/4/2016.
+ */
+public class CC_Fact_4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Integer No_of_inputs=sc.nextInt();
+        for(int i=1;i<=No_of_inputs;i++){
+            
+        }
+
+
+
+    }
+}
