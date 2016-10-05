ac9f20a22e02b03e67e0f9d05eec4a20e60c82cd
JyotendraPallav <Jyotendra@gmail.com>
2016-10-05 19:50:49 +0530

    Codes updated 5th Oct

--- learnjava2/src/main/java/com/example/CC_EIT_3.java
+++ learnjava2/src/main/java/com/example/CC_EIT_3.java
@@ -1,9 +1,5 @@
 package com.example;
 
-import com.sun.org.apache.xpath.internal.operations.Div;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -12,17 +8,15 @@ import java.util.Scanner;
 class CC_EIT_3 {
     public static void main(String[] args) throws java.lang.Exception{
         Scanner sc=new Scanner(System.in);
-        Long no_of_Inputs=sc.nextLong();
-        Long Divisor=sc.nextLong();
-        Long Limit=(long) Math.pow(10,9);
+        Integer no_of_Inputs=sc.nextInt();
+        Integer Divisor=sc.nextInt();
         Integer Counter=0;
-        if((Divisor >0) && (Divisor<=Math.pow(10,7))){
             for(int i=1;i<=no_of_Inputs;i++){
-                Long number=sc.nextLong();
-                if(number>Limit) break;
-                if(number%Divisor==0 ) Counter++;
+                if(sc.hasNextInt()) {
+                    Integer number = sc.nextInt();
+                    if (number % Divisor == 0) Counter++;
+                }
             }
-        }
         System.out.println(Counter);
     }
 }
--- learnjava2/src/main/java/com/example/CC_Fact_4.java
+++ learnjava2/src/main/java/com/example/CC_Fact_4.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -9,11 +10,23 @@ public class CC_Fact_4 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         Integer No_of_inputs=sc.nextInt();
-        for(int i=1;i<=No_of_inputs;i++){
-            
+        int Inputs[]=new int[No_of_inputs];
+        int Output[]= new int[No_of_inputs];
+        for(int i=0;i<No_of_inputs;i++){
+            Inputs[i] = sc.nextInt();
+            Output[i]=fact_zero(Inputs[i]);
         }
-
-
-
+        for(int j=0; j<No_of_inputs;j++){
+            System.out.println(Inputs[j]+"! = "+Output[j]);
+        }
+    }
+    public static int fact_zero(int n){
+        int val=1;
+        int counter=0;
+        for(int i=2;i<=n;i++){
+            val=val*i;
+            System.out.println(val);
+        }
+        return val;
     }
 }
