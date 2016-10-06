aaa2aa1eb3e4b6fa27dac380806aadf4c5b84ff7
JyotendraPallav <Jyotendra@gmail.com>
2016-10-06 19:30:01 +0530

    corrected the code fact -4

--- learnjava2/src/main/java/com/example/CC_Fact_4.java
+++ learnjava2/src/main/java/com/example/CC_Fact_4.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /**
  * Created by A0262 on 10/4/2016.
  */
-public class CC_Fact_4 {
+ class CC_Fact_4 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         if(sc.hasNextInt()) {
