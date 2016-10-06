689cef6a54684679966444dc9047379fcd1393a1
JyotendraPallav <Jyotendra@gmail.com>
2016-10-06 11:02:12 +0530

    For codechef-factorial code

--- learnjava2/src/main/java/com/example/CC_Fact_4.java
+++ learnjava2/src/main/java/com/example/CC_Fact_4.java
@@ -9,15 +9,16 @@ import java.util.Scanner;
 public class CC_Fact_4 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        Integer No_of_inputs=sc.nextInt();
-        int Inputs[]=new int[No_of_inputs];
-        int Output[]= new int[No_of_inputs];
-        for(int i=0;i<No_of_inputs;i++){
-            Inputs[i] = sc.nextInt();
-            Output[i]=fact_zero(Inputs[i]);
-        }
-        for(int j=0; j<No_of_inputs;j++){
-            System.out.println("# of Zeros in "+ Inputs[j]+"! = "+Output[j]);
+        if(sc.hasNextInt()) {
+            Integer No_of_inputs = sc.nextInt();
+            int Output[] = new int[No_of_inputs];
+            for (int i = 0; i < No_of_inputs; i++) {
+                Output[i] = fact_zero(sc.nextInt());
+            }
+            for (int j = 0; j < No_of_inputs; j++) {
+                System.out.println(Output[j]);
+                ;
+            }
         }
     }
     public static int fact_zero(int n){
